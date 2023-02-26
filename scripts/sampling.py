1af22521c6ec9635f35a4a4cd90ccee8fe736fc4
ian <h.yin@umail.leidenuniv.nl>
2023-02-26 19:41:26 +0100

    2D x sampling

--- scripts/sampling.py
+++ scripts/sampling.py
@@ -2,8 +2,8 @@ import os
 
 from pflacco.sampling import create_initial_sample
 
-dim = 10
-f = open("results/samplingX.txt", "w")
+dim = 2
+f = open("results/samplingX_2D.txt", "w")
 for i in range(100):
     X = create_initial_sample(dim, n=1000,
                               lower_bound=[-100.0 for _ in range(dim)],
